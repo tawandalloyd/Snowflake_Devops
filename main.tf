0ba7e71535cf7b077b29ebb528e144a4d82ede51
tawandalloyd <tawandalloydcharuka@gmail.com>
2025-01-06 11:21:00 +0200

    dev and prod dwh

--- main.tf
+++ main.tf
@@ -23,3 +23,14 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+
+
+resource "snowflake_database" "dev_db" {
+  name    = "ASSA_DW_DEV"
+  comment = "ASSA DEV dwh "
+}
+
+resource "snowflake_database" "prod_db" {
+  name    = "ASSA_DW_PROD"
+  comment = "ASSA DEV dwh "
+}
